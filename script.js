f2081cfba626c34e5f954fad3a20af510b21b7fe
theshayanbhutto <143422842+theshayanbhutto@users.noreply.github.com>
2023-08-28 21:08:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for block buttons
+const projectButtons = document.querySelectorAll('.project-button');
+
+projectButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('This feature is coming soon!'); // You can replace this with any desired action
+    });
+});
