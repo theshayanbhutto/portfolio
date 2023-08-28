88356e60d379a8a5e4661d32e7aa2f14ebca2418
theshayanbhutto <143422842+theshayanbhutto@users.noreply.github.com>
2023-08-28 21:52:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ projectButtons.forEach(button => {
         alert('This feature is coming soon!'); // You can replace this with any desired action
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loadingOverlay = document.querySelector(".loading-overlay");
+
+    setTimeout(() => {
+        loadingOverlay.style.opacity = 0;
+        loadingOverlay.style.pointerEvents = "none"; // Allow interaction with main content
+    }, 3000); // Adjust the delay based on your preference
+
+    const logo = document.querySelector(".logo");
+    logo.style.transform = "scale(1)";
+
+    setTimeout(() => {
+        logo.style.transform = "scale(1)";
+        logo.style.transition = "transform 0.5s ease-in-out";
+    }, 500); // Adjust the delay based on your preference
+});
+
